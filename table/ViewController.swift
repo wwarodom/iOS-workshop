a51ea4f10a54377cd5f5bf5088e84e74e748a528
CoE <coe@Kullawats-Mac.local>
2015-01-24 20:23:36 -0800

    Initial Commit

---
+++ table/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  table
+//
+//  Created by CoE on 1/24/2558 BE.
+//  Copyright (c) 2558 CoE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
